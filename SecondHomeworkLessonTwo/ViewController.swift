4e0efe914b53f83398d17c0b298faa7704ec8489
CemenVy <semen.vydrin0108@gmail.com>
2023-07-22 10:16:54 +0400

    Added UI elements and started working with logic

--- SecondHomeworkLessonTwo/ViewController.swift
+++ SecondHomeworkLessonTwo/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redSignalView: UIView!
+    @IBOutlet var yellowSignalView: UIView!
+    @IBOutlet var greenSignalView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redSignalView.layer.cornerRadius = 75
+        yellowSignalView.layer.cornerRadius = 75
+        greenSignalView.layer.cornerRadius = 75
     }
 
 
+    @IBAction func signalSwitchingButton(_ sender: UIButton) {
+    }
 }
 
