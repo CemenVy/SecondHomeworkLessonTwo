ca072ddd4b71f710b0b081ceeec7b56bb100b3be
CemenVy <semen.vydrin0108@gmail.com>
2023-07-23 15:36:37 +0400

    Work with logic

--- SecondHomeworkLessonTwo/ViewController.swift
+++ SecondHomeworkLessonTwo/ViewController.swift
@@ -7,21 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
     
-    @IBOutlet var redSignalView: UIView!
-    @IBOutlet var yellowSignalView: UIView!
-    @IBOutlet var greenSignalView: UIView!
+    @IBOutlet private var redSignalView: UIView!
+    @IBOutlet private var yellowSignalView: UIView!
+    @IBOutlet private var greenSignalView: UIView!
     
+    private var switchingAmount = 0
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        redSignalView.layer.cornerRadius = 75
-        yellowSignalView.layer.cornerRadius = 75
-        greenSignalView.layer.cornerRadius = 75
+        redSignalView.alpha = 0.3
+        yellowSignalView.alpha = 0.3
+        greenSignalView.alpha = 0.3
+        
+        redSignalView.layer.cornerRadius = redSignalView.bounds.width / 2.0
+        yellowSignalView.layer.cornerRadius = redSignalView.bounds.width / 2.0
+        greenSignalView.layer.cornerRadius = redSignalView.bounds.width / 2.0
+    }
+    
+    @IBAction private func signalSwitchingButtonDidTapped(_ sender: UIButton) {
+        
+        if switchingAmount == 0 {
+            redSignalView.alpha = 1.0
+            sender.setTitle("NEXT", for: .normal)
+            switchingAmount += 1
+        } else if switchingAmount == 1 {
+            redSignalView.alpha = 0.3
+            yellowSignalView.alpha = 1.0
+            switchingAmount += 1
+        } else if switchingAmount == 2 {
+            yellowSignalView.alpha = 0.3
+            greenSignalView.alpha = 1.0
+            switchingAmount += 1
+        } else {
+            greenSignalView.alpha = 0.3
+            redSignalView.alpha = 1.0
+            switchingAmount = 1
+        }
     }
 
-
-    @IBAction func signalSwitchingButton(_ sender: UIButton) {
-    }
 }
 
